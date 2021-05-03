5a9f5db82fa17861d0b86d328ca511486b23db01
webdevelopua <js.develop.com@gmail.com>
2021-05-03 21:16:39 +0300

    Refactor the code of Social links component

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,51 +1,47 @@
-import React from "react"
+import React from "react";
 import {
   FaFacebookSquare,
   FaLinkedin,
   FaDribbbleSquare,
   FaBehanceSquare,
   FaTwitterSquare
-} from "react-icons/fa"
+} from "react-icons/fa";
 
 const data = [
   {
     id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
+    icon: <FaFacebookSquare className="social-icon" />,
     url: "https://www.twitter.com"
   },
   {
     id: 2,
-    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    icon: <FaLinkedin className="social-icon" />,
     url: "https://www.twitter.com"
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
+    icon: <FaDribbbleSquare className="social-icon" />,
     url: "https://www.twitter.com"
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
+    icon: <FaBehanceSquare className="social-icon" />,
     url: "https://www.twitter.com"
   },
   {
     id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    icon: <FaTwitterSquare className="social-icon" />,
     url: "https://www.twitter.com"
   }
-]
-const links = data.map(link => {
-  return (
-    <li key={link.id}>
-      <a href={link.url} className="social-link">
-        {link.icon}
-      </a>
-    </li>
-  )
-})
+];
+const links = data.map(link => <li key={link.id}>
+    <a href={link.url} className="social-link">
+      {link.icon}
+    </a>
+  </li>
+);
 
-export default ({ styleClass }) => {
-  return (
-    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
-  )
-}
+
+export default ({ styleClass }) => <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
+
+
