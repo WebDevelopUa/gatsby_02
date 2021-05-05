0c64969530d2d9f56299558a20e5c9ca3374e09b
webdevelopua <js.develop.com@gmail.com>
2021-05-05 22:30:16 +0300

    Render Blog Page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,32 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 import Blogs from "../components/Blogs";
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <Layout>
-    <h2>blog page</h2>
+export const query = graphql`
+  {
+    allStrapiBlogSections {
+      nodes {
+        slug
+        description
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Blog = ({ data: { allStrapiBlogSections: { nodes: blogs } } }) =>
+  <Layout>
+    <section className="blog-page">
+      <Blogs blogs={blogs} title={`blog page`} />
+    </section>
   </Layout>;
-};
 
 export default Blog;
