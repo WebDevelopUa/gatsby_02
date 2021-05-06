2fbf2e9cc3e8f1a9206683f980052729a5a6e7f8
webdevelopua <js.develop.com@gmail.com>
2021-05-06 16:03:16 +0300

    Add 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,18 @@ import Layout from "../components/Layout";
 import { Link } from "gatsby";
 import SEO from "../components/SEO";
 
-const Error = () => {
-  return <Layout>
-    <h2>error page</h2>
-  </Layout>;
-};
+const Error = () => <Layout>
+  <main className="error-page">
+    <div className="error-container">
+      <h1>404</h1>
+      <h2>Error page</h2>
+      <br />
+      <br />
+      <Link to={`/`} className="btn">
+        back home
+      </Link>
+    </div>
+  </main>
+</Layout>;
 
 export default Error;
