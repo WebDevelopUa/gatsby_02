70704fe1563a71dde9c4e494881d36ab26be1954
webdevelopua <js.develop.com@gmail.com>
2021-05-06 15:50:37 +0300

    Add toggle action to Sidebar menu for mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,17 @@ import logo from "../assets/logo.svg";
 import { FaAlignRight } from "react-icons/fa";
 import PageLinks from "../constants/links";
 
-const Navbar = () => {
-  return <nav className="navbar">
+const Navbar = ({ toggleSidebar }) =>
+  <nav className="navbar">
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="logo" />
         <button type="button" className="toggle-btn">
-          <FaAlignRight />
+          <FaAlignRight onClick={toggleSidebar} />
         </button>
       </div>
       <PageLinks styleClass="nav-links" />
     </div>
   </nav>;
-};
 
 export default Navbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,21 +3,15 @@ import Links from "../constants/links";
 import SocialLinks from "../constants/socialLinks";
 import { FaTimes } from "react-icons/fa";
 
-const Sidebar = () => {
-  // const isOpen = false;
-  const isOpen = true;
-
-  return (
-    <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
-      <button className="close-btn">
-        <FaTimes />
-      </button>
-      <div className="side-container">
-        <Links styleClass="sidebar-links" />
-        <SocialLinks styleClass="sidebar-icons" />
-      </div>
-    </aside>
-  );
-};
+const Sidebar = ({ isOpen, toggleSidebar }) =>
+  <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
+    <button className="close-btn" onClick={toggleSidebar}>
+      <FaTimes />
+    </button>
+    <div className="side-container">
+      <Links styleClass="sidebar-links" />
+      <SocialLinks styleClass="sidebar-icons" />
+    </div>
+  </aside>;
 
 export default Sidebar;
