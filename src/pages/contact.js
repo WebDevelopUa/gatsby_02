78e4ccf6986629202138d74e8b05558d1c37dca9
webdevelopua <js.develop.com@gmail.com>
2021-05-07 14:34:04 +0300

    Add Contact page with backend service formspree.io

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const contact = () => {
-  return <Layout>
-    <h2>contact page</h2>
+const contact = () =>
+  <Layout>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>get in touch</h3>
+        <form action="https://formspree.io/f/mayaqojy" method="POST">
+          <div className="form-group">
+            <input type="text" name="name" placeholder="name" className="form-control" required />
+            <input type="email" name="email" placeholder="email" className="form-control" required />
+            <textarea name="message" placeholder="message" rows="5" className="form-control" required />
+            <button type="submit" className="btn submit-btn">Submit</button>
+          </div>
+        </form>
+      </article>
+    </section>
   </Layout>;
-};
 
 export default contact;
